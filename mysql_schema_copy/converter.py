a96745e5f95b6a7a0d61dad268ea89e14bbf7100
lazmond3 <moikilo00+xcode@gmail.com>
2021-02-07 22:36:34 +0900

    add converter progress

--- mysql_schema_copy/converter.py
+++ mysql_schema_copy/converter.py
@@ -1,13 +1,20 @@
 # TODO
+import re
 
 # スキーマのstringから、フィールドを取得する。
+
+
 def get_fields_from_schema(schema_str):
     pass
 
 # スネークケースからキャメルケースに変換する
+
+
 def get_fields_in_camel_from_schema(schema_str):
     pass
 
 # ダミーの insert を作成する。
+
+
 def get_dummy_insert_from_schema(schema_str):
     pass
