d1b3ce8ffd97717628465ca53ad7d0fe5ea2d1f1
lazmond3 <moikilo00+xcode@gmail.com>
2021-02-08 10:44:45 +0900

    add converter string

--- mysql_schema_copy/converter.py
+++ mysql_schema_copy/converter.py
@@ -15,14 +15,51 @@ def get_fields_from_schema(schema_str):
 
     return ans
 
-# スネークケースからキャメルケースに変換する
-
 
 def get_fields_in_camel_from_schema(schema_str):
+    """フィールド名の一覧を camel case で取得する
+    (手動でmybatisしたり、SELECT文を作るため)
+
+    Args:
+        schema_str (str): スキーマ定義
+    """
     pass
 
-# ダミーの insert を作成する。
+
+def get_fields_in_snake_from_schema(schema_str):
+    """フィールド名の一覧をsnake caseで取得する
+
+    Args:
+        schema_str (str): スキーマ定義
+    """
+    pass
 
 
-def get_dummy_insert_from_schema(schema_str):
+def get_insert_in_mybatis_simple(schema_str):
+    """mybatis 単数の Insertを作成する
+
+    Args:
+        schema_str (str): CREATE SCHEM
+    """
+    pass
+
+
+def get_insert_in_mybatis_bulk(schema_str):
+    """mybatis bulk の Insertを作成する
+
+    Args:
+        schema_str (str): CREATE SCHEM
+    """
+    pass
+
+
+# src[/Users/JP26446/github/react/create-insert]
+def get_dummy_insert_sql_from_schema(schema_str):
+    """dummy のsql文を作成する(最難関)
+    (すでにjsで定義しているもの)
+    ref: /Users/JP26446/github/react/create-insert
+
+    Args:
+        schema_str (str): スキーマ定義
+    """
     pass
